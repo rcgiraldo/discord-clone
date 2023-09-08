5990fef5937eb36525c0aa3b990130bce8414681
Ruben Giraldo <ru.giraldo@gmail.com>
2023-09-08 08:43:38 -0500

    theme and prisma configuration

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,8 +1,10 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { UserButton } from "@clerk/nextjs/app-beta";
 export default function Home() {
   return (
     <div>
       <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
     </div>
   );
 }
